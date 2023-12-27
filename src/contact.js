601ccfe71e2348cd55a3c34a411aa952681052ce
Vincent Testagrossa <lancevancevt@gmail.com>
2023-12-27 16:56:36 -0500

    added scaling for mobile devices. Added contact page.

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,21 @@ function createContact(){
     //TODO: Create a contact page with fake phone number,
     //email address.
     const contact = document.createElement('div');
+    const title = document.createElement('h1');
+    const phone = document.createElement('p');
+    const email = document.createElement('a');
+
+    title.textContent = "Contact us!"
+    phone.textContent = "Phone: 865-000-0000";
+    email.textContent = "support@meatomallys.com"
+    email.setAttribute('href', "mailto:support@meatomallys.com");
+    email.classList.add("link");
     contact.classList.add('contact-container');
+    contact.classList.add('transparent');
+    contact.classList.add('medium-font');
+    contact.appendChild(title);
+    contact.appendChild(phone);
+    contact.appendChild(email);
 
     return contact;
 }
